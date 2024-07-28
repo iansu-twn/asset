3af26a6715191850e2945a4645a100750df231d5
iansu-twn <iansu.job@gmail.com>
2024-07-29 00:23:26 +0800

    Update: change screenshot position to debug

--- src/grabber/Ipost.py
+++ src/grabber/Ipost.py
@@ -34,15 +34,22 @@ class Ipost(Asset):
                 btn_msg.click()
             except TimeoutException:
                 pass
-            self.driver.save_screenshot("./asset/src/grabber/login.png")
-            # change login method
-            btn_change = WebDriverWait(self.driver, 10).until(
-                EC.element_to_be_clickable(
-                    (By.XPATH, "//*[@id='content_wh']/div[1]/div/ul/li[1]/a")
-                )
-            )
-            btn_change.click()
 
+            try:
+                # change login method
+                btn_change = WebDriverWait(self.driver, 10).until(
+                    EC.element_to_be_clickable(
+                        (
+                            By.XPATH,
+                            "//*[@id='content_wh']/div[1]/div/ul/li[1]/a",
+                        )  # noqa:E501
+                    )
+                )
+                btn_change.click()
+            except TimeoutException:
+                pass
+
+            self.driver.save_screenshot("./asset/src/grabber/login.png")
             id = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, "//*[@id='cifID']"))
             )
