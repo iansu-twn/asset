bc36df9a91dab51898964d32fa30a08dbfd03903
iansu-twn <iansu.job@gmail.com>
2024-07-28 18:26:03 +0800

    Update: add selenium window size option

--- src/grabber/Asset.py
+++ src/grabber/Asset.py
@@ -17,6 +17,7 @@ class Asset:
         # configure chrome options
         options = Options()
         options.add_argument("--headless")
+        options.add_argument("--window-size=1920,1080")
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
